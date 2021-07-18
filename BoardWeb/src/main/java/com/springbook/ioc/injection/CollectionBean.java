134ffe7d90fe60d4feaa4d3f3be952a96b3d6d19
JooBoa <joocong2265@gmail.com>
2021-07-18 23:00:42 +0900

    Day1

---
+++ BoardWeb/src/main/java/com/springbook/ioc/injection/CollectionBean.java
@@ -0,0 +1,15 @@
+package com.springbook.ioc.injection;
+
+import java.util.Map;
+import java.util.Set;
+
+public class CollectionBean {
+	private Map<String ,String> addressList;
+	
+	public void setAddressList(Map<String, String>addressList) {
+		this.addressList = addressList;
+	}
+	public Map<String ,String> getAddressList(){
+		return addressList;
+	}
+}
