c3d842964f7b553a080bb8bda1218f577f2d8a5f
JooBoa <joocong2265@gmail.com>
2021-07-24 21:16:03 +0900

    P.208까지(class 05. 어노테이션 기반 AOP)

--- BoardWeb/src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
@@ -1,8 +1,16 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.JoinPoint;
-
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+@Service
+@Aspect
 public class AfterThrowingAdvice {
+	
+	
+	@AfterThrowing(pointcut="PointcutCommon.allPointcut()",throwing="exceptObj")
 	public void exceptionLog(JoinPoint jp, Exception exceptObj) {
 		String method = jp.getSignature().getName();
 
@@ -13,7 +21,7 @@ public class AfterThrowingAdvice {
 
 		if (exceptObj instanceof IllegalArgumentException) {
 			System.out.println("부적합한 값이 입력되었습니다.");
-		} else if (exceptObj instanceof IllegalArgumentException) {
+		} else if (exceptObj instanceof NumberFormatException) {
 			System.out.println("숫자 형식의 값이 아닙니다.");
 		} else if (exceptObj instanceof IllegalArgumentException) {
 			System.out.println("문제가 발생했습니다.");
