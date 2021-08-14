1a8f2d84f6d6dd3d602862b7935436cb16ff5c08
JooBoa <joocong2265@gmail.com>
2021-08-15 00:39:22 +0900

    Revert "P.464(Day 04 class 07 데이터 변환 )"
    This reverts commit 83c3ff2b67b058eec824ee0df1b45fecec8f411b.

--- BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
+++ BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
@@ -3,7 +3,6 @@ package com.springbook.view.board;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.springbook.biz.board.BoardListVO;
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
 
@@ -24,17 +21,6 @@ import com.springbook.biz.board.BoardVO;
 public class BoardController {
 	@Autowired
 	private BoardService boardService;
-	
-	@RequestMapping("/dataTransform.do")
-	@ResponseBody
-	public BoardListVO dataTransform(BoardVO vo){
-		vo.setSearchCondition("TITLE");
-		vo.setSearchKeyword("");
-		List<BoardVO> boardList = boardService.getBoardList(vo);
-		BoardListVO boardListVO = new BoardListVO();
-		boardListVO.setBoardList(boardList);
-		return boardListVO;
-	}
 	
 	// 글 등록
 	@RequestMapping("/insertBoard.do")
